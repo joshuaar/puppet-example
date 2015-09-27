daea28407f895f7c1d0512f3f819bfc129901f79
Josh Richer <josh.richer@orionhealth.com>
2015-09-26 22:03:44 -0700

    interval

--- spark-cluster/sparkworker.sh
+++ spark-cluster/sparkworker.sh
@@ -8,7 +8,7 @@ echo "192.168.33.12 sparkworker" >> /etc/hosts
 
 #configuration changes
 sudo sed -i '/\[main\]/a \ \ \ \ server=puppetmaster' /etc/puppet/puppet.conf
-sudo sed -i '/\[main\]/a \ \ \ \ runinterval=0' /etc/puppet/puppet.conf
+#sudo sed -i '/\[main\]/a \ \ \ \ runinterval=0' /etc/puppet/puppet.conf
 
 #begin polling
 sudo puppet resource service puppet ensure=running enable=true
