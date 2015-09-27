69a4d216cafec7cfe5536974d74060d972bb27fc
Josh Richer <josh.richer@orionhealth.com>
2015-09-26 20:35:06 -0700

    update

--- spark-cluster/puppetmaster.sh
+++ spark-cluster/puppetmaster.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
 sudo yum -y install puppet-server
-echo "192.168.33.10 puppetmaster" > /etc/hosts
-echo "192.168.33.11 sparkmaster" > /etc/hosts
+
+#won't need to do this if dns server is present
+echo "192.168.33.10 puppetmaster" >> /etc/hosts
+echo "192.168.33.11 sparkmaster" >> /etc/hosts
+echo "192.168.33.12 sparkworker" >> /etc/hosts
+
+#give puppetmaster all the dns names it could be contacted with
+sudo sed -i '/\[main\]/a \ \ \ \ dns_alt_names=puppetmaster' /etc/puppet/puppet.conf
+
+#start the puppet master
+sudo puppet master
--- spark-cluster/sparkmaster.sh
+++ spark-cluster/sparkmaster.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 sudo yum -y install puppet
-echo "192.168.33.10 puppetmaster" > /etc/hosts
-echo "192.168.33.11 sparkmaster" > /etc/hosts
+
+#won't need to do this if dns server is present
+echo "192.168.33.10 puppetmaster" >> /etc/hosts
+echo "192.168.33.11 sparkmaster" >> /etc/hosts
+echo "192.168.33.12 sparkworker" >> /etc/hosts
+
+#configuration changes
+sudo sed -i '/\[main\]/a \ \ \ \ server=puppetmaster' /etc/puppet/puppet.conf
+sudo sed -i '/\[main\]/a \ \ \ \ runinterval=0' /etc/puppet/puppet.conf
+
+#begin polling
+sudo puppet resource service puppet ensure=running enable=true
